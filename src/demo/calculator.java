37276326e831c750397a40553e38d38aa1661a55
Pranoti-Kondabattin <pranotikondabattin27@gmail.com>
2022-08-18 15:57:15 +0530

    added math and calculator class

---
+++ src/demo/calculator.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class calculator 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Addition");
+		math.add(22,67);
+		math.add(65,89,43);
+		System.out.println("Multiplication");
+		math.multiply(23,8);
+		
+	}
+}
